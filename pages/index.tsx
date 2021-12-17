34ecca49b75fc5e70c730e07074f649bb232b234
Thomas Maqua <thomasmaqua@gmail.com>
2021-12-17 11:05:11 +0100

    Setup typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -13,11 +13,11 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing <code>pages/index.tsx</code>
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
